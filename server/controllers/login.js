a0b58c16495871c143900971405c7623622b33b3
AnGaoLang <1774087845@qq.com>
2019-04-17 20:24:11 +0800

    注册和登陆

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -6,25 +6,35 @@ const userModel = require('../models/userInfo');
 // 用户名登录系统只涉及非github用户，也就是github用户只能走github授权来登录
 module.exports = async (ctx, next) => {
   const { name = '', password = '' } = ctx.request.body;
-  if (name === '' || password === '') {
-    ctx.body = {
-      success: false,
-      message: '用户名或密码不能为空'
-    };
-    return;
-  }
-  const RowDataPacket = await userModel.findDataByName(name);
+  // 用户、密码为空可以直接在前端验证
+  // if (name === '' || password === '') {
+  //   ctx.response.body = {
+  //     success: false,
+  //     message: '用户名或密码不能为空'
+  //   };
+  //   return;
+  // };
+  
+  const RowDataPacket = await userModel.findDataByName(name); // 通过sql查找数据库
   const res = JSON.parse(JSON.stringify(RowDataPacket));
+
+  // 数据库中查找到相应的用户名
   if (res.length > 0) {
     //   验证成功后，服务端会签发一个 Token，再把这个 Token 发送给客户端
-    if (md5(password) === res[0].password) { // MD5加密
+    if (md5(password) === res[0].password) { // MD5加密，数据库里存的也是经过加密后的密码
+
+      // 取出字段
       const {
         id, name, sex, website, github, intro, company, avatar, location, socketId
       } = res[0];
+
+      // 签发一个token
       const payload = { id };
       const token = jwt.sign(payload, secret, {
         expiresIn: Math.floor(Date.now() / 1000) + 24 * 60 * 60 * 7 // 一周
       });
+
+      // 返回前端json字符串
       ctx.body = {
         success: true,
         message: '登录成功',
@@ -42,13 +52,13 @@ module.exports = async (ctx, next) => {
           token
         }
       };
-    } else {
+    } else { // 密码不对。返回密码错误
       ctx.body = {
         success: false,
         message: '密码错误'
       };
     }
-  } else {
+  } else { // 未在数据库中查找到数据，则用户名错误
     ctx.body = {
       success: false,
       message: '用户名错误'
--- server/controllers/register.js
+++ server/controllers/register.js
@@ -2,23 +2,49 @@ const md5 = require('md5');
 const userModel = require('../models/userInfo');
 
 module.exports = async (ctx, next) => {
-  console.log('register');
+  
   const { name, password } = ctx.request.body;
-
-  await userModel.findDataByName(name).then((result) => {
-    console.log(result);
-    if (result.length) {
-      ctx.body = {
+  
+  let res = await userModel.findDataByName(name).then((result) => {
+    if (result.length) { // 如果依据用户名查出数据，则用户名已存在
+      // 返回信息
+      ctx.response.body = {
         success: false,
         message: '用户名已存在'
       };
     } else {
-      ctx.body = {
-        success: true,
-        message: '注册成功！'
-      };
-      console.log('注册成功');
-      userModel.insertData([name, md5(password)]);
+      return 0;
     }
   });
+  if (!res) {
+    await userModel.insertData([name, md5(password)]).then((result1) => {
+      ctx.response.body = {
+        success: true,
+        message: '注册成功！'
+      }
+    });
+  }
+  
+  // await let res = userModel.findDataByName(name).then((result) => {
+  //   // console.log(result);
+  //   if (result.length) { // 如果依据用户名查出数据，则用户名已存在
+  //     // 返回信息
+  //     ctx.response.body = {
+  //       success: false,
+  //       message: '用户名已存在'
+  //     };
+  //   } else {
+  //     console.log(111)
+  //     // 如果未查到，则将用户和密码（经过md5加密）写入数据库
+  //     userModel.insertData([name, md5(password)]).then((result1) => {
+  //       console.log(result1);
+  //       ctx.response.body = {
+  //         success: true,
+  //         message: '注册成功！'
+  //       }
+  //       console.log(222)
+  //     });
+  //   }
+
+  // });
 };
